326cc45a164265c7b0d4f84abb9e22e526eb0e68
Saurabh Kumar <84388992+saurabh1505717@users.noreply.github.com>
2023-12-26 11:10:44 +0000

    conversionOperation

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,23 @@
+let score = "33ab"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNUmber = Number(score);
+// console.log(typeof valueInNUmber);
+// console.log(valueInNUmber);
+
+// "33" => 33
+// "33ab" => NaN
+// true => 1; false => 0
+
+// let isLoggedIn=1;
+let isLoggedIn="saurabh";
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+
+// 1 => true; 0 => false 
+// "" => false 
+// "Saurabh" => true
